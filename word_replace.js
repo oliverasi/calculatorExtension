b1ab80525fdd39318acbabf8298ff6a0aa8288bc
Ben <123242974+Bengcodes@users.noreply.github.com>
2023-05-13 23:57:33 +1000

    Add files via upload

---
+++ word_replace.js
@@ -0,0 +1,15 @@
+const text = document.querySelectorAll
+('h1, h2, h2, h4, h5, p, li, td, caption, span, a')
+
+for(let i=0; i<text.length; i++) {
+    if(text[i].innerHTML.includes('Assi')) {
+        text[i].innerHTML = text[i].innerHTML.
+        replace('Assi', 'Ass')
+    } else if(text[i].innerHTML.includes('ASSI')) {
+        text[i].innerHTML = text[i].innerHTML.
+        replace('ASSI', 'Ass')
+    } else if(text[i].innerHTML.includes('assi')) {
+            text[i].innerHTML = text[i].innerHTML.
+            replace('assi', 'Ass')
+    }
+}
